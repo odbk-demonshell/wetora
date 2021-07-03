19307137f42be265f2df554254078fde6db7b9b8
ODBK <odbk@demonshell.com>
2021-07-03 06:06:40 +0200

    Clearing org list

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from printer import printer
 from transformations import transformations
 
 info = printer()
-info.banner("Wetora","ODBK", "DEMONSHELL")
+info.banner("Wetora","ODBK", " DEMONSHELL ")
 
 # Defining a target 
 if len(sys.argv) == 2: 
@@ -13,6 +13,10 @@ if len(sys.argv) == 2:
 else: 
     print("Invalid ammount of Argument") 
 
+'''
+## BEGIN ##
+'''
+# FROM DOMAIN TO ORGANIZATION NAME
 scope = transformations()
 try:
     name = scope.domainToOrg(target)
@@ -25,3 +29,27 @@ try:
 except:
     info.error("  WHOIS ERROR  ")
     info.equit()
+
+# FROM ORG NAME TO NAMELIST
+try:
+    print("Searching similars organizations: ")
+    nameList = scope.orgToNameList("google")
+    if( nameList ):
+        #Clearing duplicates
+        nameList = list(dict.fromkeys(nameList))
+        for number, name in enumerate(nameList):
+            info.highlight("["+str(number)+"] - ", name)
+        #Remove unwanted organizations
+        remove = list(map(int, input("Do you want remove any item? ").split(",")))
+        nameList = [i for j, i in enumerate(nameList) if j not in remove]
+        for number, name in enumerate(nameList):
+            info.highlight("["+str(number)+"] - ", name)
+    else:
+        info.ehighlight("Similars Organizations: ", "NULL")
+        info.equit()
+
+except:
+    info.error("  ERROR GETTING LIST  ")
+    info.equit()
+
+
--- transformations.py
+++ transformations.py
@@ -1,11 +1,14 @@
 #!/usr/bin/env python
 import whois # pip install python-whois
-from printer import printer
 import sys
+import requests
+import json
+import xml.etree.ElementTree as ET
 
 class transformations:
 	def __init__(self):
-		self.alert = printer()
+		self.ripeUrl = "http://rest.db.ripe.net/search?source=ripe&source=lacnic-grs&source=apnic-grs&source=afrinic-grs" \
+						"&source=arin-grs&source=jpirr-grs&source=radb-grs&query-string="
 
 	def domainToOrg(self, domain_name):
 		try:
@@ -17,7 +20,19 @@ class transformations:
 			raise
 		return None
 
-
+	def orgToNameList(self, org_name):
+		try:
+			response = requests.get(self.ripeUrl+org_name)
+			tree = ET.ElementTree(ET.fromstring(response.text))
+			namelist = []
+			for node in tree.iter():
+				if(node.attrib.get("name") == "org-name"):
+					namelist.append(node.attrib.get("value"))
+			return namelist
+		except:
+			print("Unexpected error:", sys.exc_info()[0])
+			raise
+		return None
 
 # list of registered & non registered domains to test our function
 #domains = [
